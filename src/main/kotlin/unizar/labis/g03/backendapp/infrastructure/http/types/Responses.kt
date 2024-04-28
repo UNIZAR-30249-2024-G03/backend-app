ce84a0c05e472c2e50c80d5c704c870a8770dda9
juan-catalan <juancatalan2001@gmail.com>
2024-04-28 15:51:25 +0200

    Añadir controladores HTTP

---
+++ src/main/kotlin/unizar/labis/g03/backendapp/infrastructure/http/types/Responses.kt
@@ -0,0 +1,45 @@
+package unizar.labis.g03.backendapp.infrastructure.http.types
+
+import unizar.labis.g03.backendapp.model.valueObjects.Departamento
+import unizar.labis.g03.backendapp.model.valueObjects.InfoReserva
+import unizar.labis.g03.backendapp.model.valueObjects.Rol
+import unizar.labis.g03.backendapp.model.valueObjects.TipoEspacio
+
+
+class EspacioOut(
+    val id: String? = null,
+    val tamano: Float? = null,
+    val tipoEspacio: TipoEspacio? = null,
+    val categoriaReserva: TipoEspacio? = null,
+    val numMaxOcupantes: Int? = null,
+    val planta: Int? = null,
+    val reservable: Boolean? = null,
+    val porcentajeUsoMaximo: Int? = null
+) {
+    override fun toString(): String {
+        return "EspacioOut(id=$id, tamano=$tamano, tipoEspacio=$tipoEspacio, categoriaReserva=$categoriaReserva, numMaxOcupantes=$numMaxOcupantes, planta=$planta, reservable=$reservable, porcentajeUsoMaximo=$porcentajeUsoMaximo)"
+    }
+}
+
+class PersonaOut(
+    val nombre: String? = null,
+    val apellido: String? = null,
+    val email: String? = null,
+    val roles: Set<Rol>? = null,
+    val departamentoAdscrito: Departamento? = null
+) {
+    override fun toString(): String {
+        return "PersonaOut(nombre=$nombre, apellido=$apellido, email=$email, roles=$roles, departamentoAdscrito=$departamentoAdscrito)"
+    }
+}
+
+class ReservaOut(
+    val id: String? = null,
+    val persona: PersonaOut? = null,
+    val espacio: EspacioOut? = null,
+    val infoReserva: InfoReserva? = null
+) {
+    override fun toString(): String {
+        return "ReservaOut(id=$id, persona=$persona, espacio=$espacio, infoReserva=$infoReserva)"
+    }
+}
