8faf96edfec4057917c32ac0f8c91d082ebf8d93
juan-catalan <juancatalan2001@gmail.com>
2024-05-04 20:06:17 +0200

    Añadir datos reales de la planta 1-4 del ada byron

---
+++ sql/create_views.sql
@@ -0,0 +1,158 @@
+-- public.planta0 source
+
+CREATE OR REPLACE VIEW public.planta0
+AS SELECT geom,
+    id,
+    "Link",
+    "ID_EDIF",
+    "Centro",
+    "SUPERFICIE",
+    "Nombre",
+    "ID_UXXI",
+    "FOTO_360",
+    "FOTO_PLANA",
+    "USO",
+    "EDIFICIO",
+    "Altura",
+    "Dummy",
+    "Shape__Area",
+    "Shape__Length",
+    categoria_reserva,
+    hora_apertura,
+    hora_cierre,
+    tipo_espacio,
+    num_max_ocupantes,
+    planta,
+    porcentaje_uso_maximo,
+    reservable,
+    tamano
+   FROM espacio e
+  WHERE planta = 0;
+
+
+-- public.planta1 source
+
+CREATE OR REPLACE VIEW public.planta1
+AS SELECT geom,
+    id,
+    "Link",
+    "ID_EDIF",
+    "Centro",
+    "SUPERFICIE",
+    "Nombre",
+    "ID_UXXI",
+    "FOTO_360",
+    "FOTO_PLANA",
+    "USO",
+    "EDIFICIO",
+    "Altura",
+    "Dummy",
+    "Shape__Area",
+    "Shape__Length",
+    categoria_reserva,
+    hora_apertura,
+    hora_cierre,
+    tipo_espacio,
+    num_max_ocupantes,
+    planta,
+    porcentaje_uso_maximo,
+    reservable,
+    tamano
+   FROM espacio e
+  WHERE planta = 1;
+
+
+-- public.planta2 source
+
+CREATE OR REPLACE VIEW public.planta2
+AS SELECT geom,
+    id,
+    "Link",
+    "ID_EDIF",
+    "Centro",
+    "SUPERFICIE",
+    "Nombre",
+    "ID_UXXI",
+    "FOTO_360",
+    "FOTO_PLANA",
+    "USO",
+    "EDIFICIO",
+    "Altura",
+    "Dummy",
+    "Shape__Area",
+    "Shape__Length",
+    categoria_reserva,
+    hora_apertura,
+    hora_cierre,
+    tipo_espacio,
+    num_max_ocupantes,
+    planta,
+    porcentaje_uso_maximo,
+    reservable,
+    tamano
+   FROM espacio e
+  WHERE planta = 2;
+
+
+-- public.planta3 source
+
+CREATE OR REPLACE VIEW public.planta3
+AS SELECT geom,
+    id,
+    "Link",
+    "ID_EDIF",
+    "Centro",
+    "SUPERFICIE",
+    "Nombre",
+    "ID_UXXI",
+    "FOTO_360",
+    "FOTO_PLANA",
+    "USO",
+    "EDIFICIO",
+    "Altura",
+    "Dummy",
+    "Shape__Area",
+    "Shape__Length",
+    categoria_reserva,
+    hora_apertura,
+    hora_cierre,
+    tipo_espacio,
+    num_max_ocupantes,
+    planta,
+    porcentaje_uso_maximo,
+    reservable,
+    tamano
+   FROM espacio e
+  WHERE planta = 3;
+
+
+-- public.planta4 source
+
+CREATE OR REPLACE VIEW public.planta4
+AS SELECT geom,
+    id,
+    "Link",
+    "ID_EDIF",
+    "Centro",
+    "SUPERFICIE",
+    "Nombre",
+    "ID_UXXI",
+    "FOTO_360",
+    "FOTO_PLANA",
+    "USO",
+    "EDIFICIO",
+    "Altura",
+    "Dummy",
+    "Shape__Area",
+    "Shape__Length",
+    categoria_reserva,
+    hora_apertura,
+    hora_cierre,
+    tipo_espacio,
+    num_max_ocupantes,
+    planta,
+    porcentaje_uso_maximo,
+    reservable,
+    tamano
+   FROM espacio e
+  WHERE planta = 4;
