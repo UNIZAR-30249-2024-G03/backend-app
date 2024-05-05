50c2d98efa05f5537fb647bbaff9783e770a6b63
juan-catalan <juancatalan2001@gmail.com>
2024-05-05 15:55:02 +0200

    Arreglar ficheros DDL

--- sql/create_tables.sql
+++ sql/create_tables.sql
@@ -38,3 +38,38 @@ CREATE TABLE public.espacio (
 	CONSTRAINT espacio_tipo_entidad_check CHECK (((tipo_entidad)::text = ANY ((ARRAY['EINA'::character varying, 'DEPARTAMENTO'::character varying, 'PERSONAS'::character varying])::text[]))),
 	CONSTRAINT espacio_tipo_espacio_check CHECK (((tipo_espacio)::text = ANY (ARRAY[('AULA'::character varying)::text, ('SEMINARIO'::character varying)::text, ('LABORATORIO'::character varying)::text, ('DESPACHO'::character varying)::text, ('SALA_COMUN'::character varying)::text])))
 );
+
+-- public.personas definition
+
+-- Drop table
+
+-- DROP TABLE public.personas;
+
+CREATE TABLE public.personas (
+	email varchar(255) NOT NULL,
+	apellido varchar(255) NULL,
+	departamento_adscrito varchar(255) NULL,
+	nombre varchar(255) NULL,
+	roles _varchar NULL,
+	CONSTRAINT personas_departamento_adscrito_check CHECK (((departamento_adscrito)::text = ANY ((ARRAY['Informatica_e_Ingenieria_de_sistemas'::character varying, 'Ingenieria_electronica_y_comunicaciones'::character varying])::text[]))),
+	CONSTRAINT personas_pkey PRIMARY KEY (email)
+);
+
+
+-- public.espacio_personas definition
+
+-- Drop table
+
+-- DROP TABLE public.espacio_personas;
+
+CREATE TABLE public.espacio_personas (
+	espacio_id varchar(255) NOT NULL,
+	personas_email varchar(255) NOT NULL,
+	CONSTRAINT uk_4eanptgi14u9w6ebxhdwx6ubo UNIQUE (personas_email)
+);
+
+
+-- public.espacio_personas foreign keys
+
+ALTER TABLE public.espacio_personas ADD CONSTRAINT fkav9g37ed36wugkaxn0uwtxvi9 FOREIGN KEY (personas_email) REFERENCES public.personas(email);
+ALTER TABLE public.espacio_personas ADD CONSTRAINT fkrivcnxype0vblqqyyuwgusw0e FOREIGN KEY (espacio_id) REFERENCES public.espacio(id);
